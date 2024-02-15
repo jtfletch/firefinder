41411911bf9c6d202fd8e171d13a408910abef7c
jtfletch <jobetfletcher@gmail.com>
2024-02-15 16:30:20 +1100

    Added advanced marker
    Need to implement mapping for nasa api call

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,16 +1,21 @@
-import {
-  APIProvider,
-  Map,
-  Marker,
-  Pin,
-} from "@vis.gl/react-google-maps";
+import { APIProvider, Map, AdvancedMarker, customMarker } from "@vis.gl/react-google-maps";
+import { Icon } from '@iconify/react'
+import locationIcon from '@iconify/icons-mdi/fire-alert'
 
 const LoadMap = () => {
   return (
     <div className="map">
       <APIProvider apiKey={process.env.REACT_APP_GOOGLE_API_KEY}>
-        <Map defaultZoom={10} defaultCenter={{ lat: 53.54992, lng: 10.00678 }} gestureHandling={"greedy"}>
-          <Marker position={{ lat: 53.54992, lng: 10.00678 }} />
+        <Map
+          defaultZoom={10}
+          defaultCenter={{ lat: 53.54992, lng: 10.00678 }}
+          gestureHandling={"greedy"}
+          mapId={"8095e43d45822679"}
+        >
+          <AdvancedMarker 
+            position={{ lat: 68.54992, lng: 10.00678 }}>
+            <Icon icon={locationIcon} className="location-icon" />
+          </AdvancedMarker>
         </Map>
       </APIProvider>
     </div>
